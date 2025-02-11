d25ec78acb5452686febf376da7c1ae9e73f26e2
Adrian Reichardt <Reichardt-A@ulster.ac.uk>
2025-02-11 12:58:42 +0000

    Updated Speed Variable, camera, changed some variable names

--- Assets/Scripts/CarInputHandler.cs
+++ Assets/Scripts/CarInputHandler.cs
@@ -1,24 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarInputHandler : MonoBehaviour
 {
-    TopDownCarController topDownCarController;
+    TopDownCarController carController;
 
     void Awake()
     {
-        topDownCarController = GetComponent<TopDownCarController>();
+        carController = GetComponent<TopDownCarController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = Vector2.zero;
         
+        Vector2 inputVector = Vector2.zero;
         inputVector.x = Input.GetAxis("Horizontal");
         inputVector.y = Input.GetAxis("Vertical");
         
-        topDownCarController.SetInputVector(inputVector);
+        
+        carController.SetInputVector(inputVector);
     }
+/*
+    public void KMmovement()
+    {
+        Vector2 inputVector = Vector2.zero;
+        inputVector.x = amongus
+        inputVector.y = sus
+    }
+
+    public void Controllermovement()
+    {
+        Vector2 inputVector = Vector2.zero;
+        inputVector.x = amongus
+        inputVector.y = sus
+    }
+    */
 }
--- Assets/Scripts/TopDownCarController.cs
+++ Assets/Scripts/TopDownCarController.cs
@@ -10,7 +10,7 @@ public class TopDownCarController : MonoBehaviour
     public float driftFactor = 0.30f;
     public float accelerationFactor = 30.0f;
     public float turnFactor = 3.5f;
-    public float maxSpeed = 20f;
+    public float maxSpeed = 75;
 
     //Local variables
     float accelerationInput = 0;
@@ -20,23 +20,11 @@ public class TopDownCarController : MonoBehaviour
 
     float velocityVsUp = 0;
 
-    Rigidbody2D carRigidbody2D;
+    Rigidbody2D rb;
 
     void Awake()
     {
-        carRigidbody2D = GetComponent<Rigidbody2D>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
@@ -51,7 +39,7 @@ public class TopDownCarController : MonoBehaviour
     void ApplyEngineForce()
     {
         //Calculate forward in terms of the direction of our velocity
-        velocityVsUp = Vector2.Dot(transform.up, carRigidbody2D.velocity);
+        velocityVsUp = Vector2.Dot(transform.up, rb.velocity);
 
         //Limit max speed in the forward direction
         if(velocityVsUp > maxSpeed && accelerationInput > 0)
@@ -66,7 +54,7 @@ public class TopDownCarController : MonoBehaviour
         }
 
         //Limit speed in any direction while accelerating
-        if(carRigidbody2D.velocity.sqrMagnitude > maxSpeed * maxSpeed && accelerationInput > 0)
+        if(rb.velocity.sqrMagnitude > maxSpeed * maxSpeed && accelerationInput > 0)
         {
             return;
         }
@@ -74,40 +62,40 @@ public class TopDownCarController : MonoBehaviour
         //applies drag if no acceleration input so this stops the car when the player lets go of the button
         if(accelerationInput == 0)
         {
-            carRigidbody2D.drag = Mathf.Lerp(carRigidbody2D.drag, 3.0f, Time.deltaTime * 3);
+            rb.drag = Mathf.Lerp(rb.drag, 3.0f, Time.deltaTime * 3);
         }
         else
         {
-            carRigidbody2D.drag = 0;
+            rb.drag = 0;
         }
 
         // create a force for the engine
       Vector2 engineForceVector = transform.up * accelerationInput * accelerationFactor;
 
       //apply the force and pushes the car forward
-      carRigidbody2D.AddForce(engineForceVector, ForceMode2D.Force);
+      rb.AddForce(engineForceVector, ForceMode2D.Force);
     }
 
     void ApplySteering()
     {
         //limit cars ability to turn while moving
-        float minSpeedBeforeTurningFactor = (carRigidbody2D.velocity.magnitude /8);
+        float minSpeedBeforeTurningFactor = (rb.velocity.magnitude /8);
         minSpeedBeforeTurningFactor = Mathf.Clamp01(minSpeedBeforeTurningFactor);
 
         //update rotation angle based on input 
         rotationAngle -= steeringInput * turnFactor * minSpeedBeforeTurningFactor;
         
         //apply steering by rotating the car object
-        carRigidbody2D.MoveRotation(rotationAngle);
+        rb.MoveRotation(rotationAngle);
     }
 
     //side ways velocity for car
     void KillOrthogonalVelocity()
     {
-        Vector2 fowardVelocity = transform.up * Vector2.Dot(carRigidbody2D.velocity, transform.up);
-        Vector2 rightVelocity = transform.right * Vector2.Dot(carRigidbody2D.velocity, transform.right);
+        Vector2 fowardVelocity = transform.up * Vector2.Dot(rb.velocity, transform.up);
+        Vector2 rightVelocity = transform.right * Vector2.Dot(rb.velocity, transform.right);
 
-        carRigidbody2D.velocity = fowardVelocity + rightVelocity * driftFactor;
+        rb.velocity = fowardVelocity + rightVelocity * driftFactor;
     }
 
     public void SetInputVector(Vector2 inputVector)
@@ -115,4 +103,5 @@ public class TopDownCarController : MonoBehaviour
         steeringInput = inputVector.x;
         accelerationInput = inputVector.y;
     }
+
 }
