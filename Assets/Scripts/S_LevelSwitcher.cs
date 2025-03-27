6de36a7ef6cf2c43009ea1f7c6126055496a9cb1
Adrian Reichardt <Reichardt-A@ulster.ac.uk>
2025-03-27 15:54:48 +0000

    s

--- Assets/Scripts/S_LevelSwitcher.cs
+++ Assets/Scripts/S_LevelSwitcher.cs
@@ -29,18 +29,23 @@ public class S_LevelSwitcher : MonoBehaviour
         switch(scene)
         {
             case Scene.MainMenu:
+                GameManager.instance.ChangeGameState(GameState.Countdown);
                 SceneManager.LoadScene(NameStrings.MainMenu);
                 break;
             case Scene.Scene1:
+                GameManager.instance.ChangeGameState(GameState.Countdown);
                 SceneManager.LoadScene(NameStrings.SingleplayerLevel1);
                 break;
             case Scene.Scene2:
+                GameManager.instance.ChangeGameState(GameState.Countdown);
                 SceneManager.LoadScene(NameStrings.SingleplayerLevel2);
                 break;
             case Scene.Scene3:
+                GameManager.instance.ChangeGameState(GameState.Countdown);
                 SceneManager.LoadScene(NameStrings.SingleplayerLevel3);
                 break;
             case Scene.Scene4:
+                GameManager.instance.ChangeGameState(GameState.Countdown);
                 SceneManager.LoadScene(NameStrings.SingleplayerLevel4);
                 break;
 
